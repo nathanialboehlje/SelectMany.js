2ac794be2b06d972b7ab5989764b9e82d15b9159
Nathanial Boehlje <nathanialboehlje255@gmail.com>
2023-04-23 16:38:32 -0500

    added css

--- SelectMany.js
+++ SelectMany.js
@@ -2,22 +2,45 @@
 // <script type="text/html">
 
 // </script>
+
+
+
 class SelectMany {
     constructor(obj) { 
         this.element = obj.element; 
         this.message = obj.message; 
         this.processElement(this.element);
-        this.showOptions(obj.element) 
+        this.renderSelect(obj.element) 
     }
     processElement(element){ 
         this.optionsText = Array.from(element.options, x => x.innerText);
         
     }
-    showOptions(el) { 
-        const newEl = document.createElement('div'); 
-        const text = this.optionsText.reduce((preVal, currentVal, index) => preVal +  ` ${index} ` + currentVal); 
-        const textNode = document.createTextNode(text); 
-        newEl.appendChild(textNode); 
+    renderSelect(el) {  
+        // create input element 
+        const newInput = this.renderInput(); 
+        this.element.insertAdjacentElement('afterend', newInput);
+       
+        // create div
+        // add input element to it
+        this.div = document.createElement('div'); 
+        this.div.setAttribute('id', 'select-many-div')
+        this.div.appendChild(newInput); 
+
+        // create results list 
+        const resultsList = this.renderList(this.optionsText); 
+        this.div.appendChild(resultsList); 
+
+        // add listener to input element 
+        // onchange -> filter options 
+        newInput.addEventListener('input', (event) => {
+            const inputVal = event.target.value; 
+            const filteredOptions = this.optionsText.filter(x => x.includes(inputVal)); 
+            this.div.removeChild(this.ul);  
+            const newList = this.renderList(filteredOptions);
+            this.div.appendChild(newList);  
+        })
+
     }
     invalid(msg){ 
         this.invalidMsg = msg; 
@@ -28,5 +51,20 @@ class SelectMany {
     }
     displayOptions(){ 
         this.optionsText.forEach(x => console.log(x)); 
+    }
+    renderInput() { 
+        this.searchInput = document.createElement('input');
+        this.searchInput.setAttribute('type', 'text'); 
+        this.searchInput.setAttribute('placeholder', 'type something...');
+        return this.searchInput;  
+    }
+    renderList(list) { 
+        this.ul = document.createElement('ul');
+        for (const item of list){  
+            let li = document.createElement('li'); 
+            li.textContent = item;  
+            this.ul.appendChild(li); 
+        }
+        return this.ul;  
     }
 }
