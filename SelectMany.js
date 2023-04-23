b8cb54d46b49ccf487662674fa96bbc09f4bb0c6
Nathanial Boehlje <nathanialboehlje255@gmail.com>
2023-04-22 19:13:31 -0500

    made changes...

--- SelectMany.js
+++ SelectMany.js
@@ -1,40 +1,32 @@
 
-// class SelectMany {
-//     constructor(obj) {
-//         this.message = obj.message;  
+// <script type="text/html">
 
-//     }
-//     get DisplayMessage(){ 
-//         window.alert(this.message); 
-//     }
-// }
-var SelectMany = { 
-    init (obj) { 
-        this.message = obj.message;
-        this.element = obj.element;  
-        this.processElement(obj.element); 
-    },
-    displayMessage() { 
-        window.alert(this.message); 
-    }, 
-    processElement(el) { 
-        const options = Object.keys(el); 
-        const selectOptions = []; 
-        options.forEach(x => { 
-            selectOptions.push(el[x].innerText); 
-        })
-
-
+// </script>
+class SelectMany {
+    constructor(obj) { 
+        this.element = obj.element; 
+        this.message = obj.message; 
+        this.processElement(this.element);
+        this.showOptions(obj.element) 
+    }
+    processElement(element){ 
+        this.optionsText = Array.from(element.options, x => x.innerText);
         
-    }, 
-    invalid(validationMsg) {
-        if (validationMsg != null) { 
-            this.validationMsg = validationMsg;
-            this.displayValMsg(); 
-        } 
-    }, 
-    displayValMsg() { 
-        window.alert(this.validationMsg); 
+    }
+    showOptions(el) { 
+        const newEl = document.createElement('div'); 
+        const text = this.optionsText.reduce((preVal, currentVal, index) => preVal +  ` ${index} ` + currentVal); 
+        const textNode = document.createTextNode(text); 
+        newEl.appendChild(textNode); 
+    }
+    invalid(msg){ 
+        this.invalidMsg = msg; 
+        window.alert(this.invalidMsg); 
+    }
+    displayMsg() { 
+        window.alert(this.message); 
+    }
+    displayOptions(){ 
+        this.optionsText.forEach(x => console.log(x)); 
     }
 }
-
